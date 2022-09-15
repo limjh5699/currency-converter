a532f8e1e141e6f7bca642139de991b8983346a9
limjh5699 <limjh5699@gmail.com>
2022-09-16 08:53:53 +0900

    Rename excahnge-rate-calculator to currency-converter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/reset.module.css'
+import './styles/App.module.css'
+import Main from './pages/currency-converter/currency-converter'
 
 function App() {
   return (
     <div>
-
+      <Main />
     </div>
   );
 }
---
+++ src/pages/currency-converter/currency-converter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './currency-converter.module.css'
+
+const main = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.box}>
+        <div className={styles.inputBox}>
+
+        </div>
+        <div className={styles.selectC}></div>
+      </div>
+    </div>
+  )
+}
+
+export default main
