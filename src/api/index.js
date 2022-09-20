fc3aab9ce937769af4d1b3d2e6c1b6a144e79bd2
limjh5699 <limjh5699@gmail.com>
2022-09-20 17:40:34 +0900

    First Complete

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY // input your api key
-const URL = `/site/program/financial/exchangeJSON?authkey=${API_KEY}&data=AP01`
+const URL = `/site/program/financial/exchangeJSON?authkey=${API_KEY}&searchdate=20220914&data=AP01`
 
 const getExchangeRate = async () => {
   return fetch(URL).then(res => res.json())
--- src/pages/currency-converter/currency-converter.js
+++ src/pages/currency-converter/currency-converter.js
@@ -1,27 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import api from '../../api'
 import styles from './currency-converter.module.css'
 import { countryList } from "../../utils/countryList";
 
 const Main = () => {
   const [convertPrice, setConvertPrice] = useState(100)
+  const [exchangePrice, setExchangePrice] = useState()
   const [selectRemittanceCountry, setSelectRemittanceCountry] = useState('KRW')
   const [selectReceptionCountry, setSelectReceptionCountry] = useState('USD')
+  const [remittanceCountryPrice, setRemittanceCountryPrice] = useState()
+  const [receptionCountryPrice, setReceptionCountryPrice] = useState()
   const [errorMassage, setErrorMessage] = useState('')
-  const [isVaild, setIsVaild] = useState(false)
+  const [isVaild, setIsVaild] =  useState(false)
+  
 
-  const checkValue = (value) => {
-    if(Number(value) < 0) {
+  useEffect(() => {
+    api().then((res) => {
+      res.map(m => {
+        if(m.cur_unit.substring(0, 3) === selectRemittanceCountry) {
+          let value = m.deal_bas_r.replace(',', '')
+          setRemittanceCountryPrice(value)
+        }
+        if(m.cur_unit.substring(0, 3) === selectReceptionCountry) {
+          let value = m.deal_bas_r.replace(',', '')
+          setReceptionCountryPrice(value)
+        }
+      })
+    })
+
+    setExchangePrice((convertPrice / (receptionCountryPrice / remittanceCountryPrice)).toFixed(5))
+    console.log(convertPrice / (receptionCountryPrice / remittanceCountryPrice))
+
+    if(Number(convertPrice) < 0) {
       setIsVaild(false)
       setErrorMessage('0 이상의 값을 입력해주세요')
-    } else if (isNaN(value)) {
+    } else if (isNaN(convertPrice)) {
       setIsVaild(false)
       setErrorMessage('숫자만 입력하세요')
     } else {
       setIsVaild(true)
       setErrorMessage('')
     }
-  }
-
+  }, [selectRemittanceCountry, selectReceptionCountry, convertPrice, receptionCountryPrice, remittanceCountryPrice])
+  
   return (
     <div className={styles.main}>
       <div className={styles.box}>
@@ -32,27 +53,23 @@ const Main = () => {
           </div>
         </div>
         <div className={styles.selectCountry}>
-          <select className={styles.remittanceCountry} onChange={(e) => setSelectRemittanceCountry(e.target.value)}>
-            <option value="KRW" selected>KRW(한국/원)</option>
-            <option value="USD">USD(미국/달러)</option>
+          <select className={styles.remittanceCountry} value={selectRemittanceCountry} onChange={(e) => setSelectRemittanceCountry(e.target.value)}>
             {countryList.map((country) => {
-              return <option value={country.split(0, 3)}>{country}</option>
+              return <option key={country.key} value={country.value.substring(0, 3)}>{country.value}</option>
             })}
           </select>
-          <select className={styles.receptionCountry} onChange={(e) => setSelectReceptionCountry(e.target.value)}>
-            <option value="KRW">KRW(한국/원)</option>
-            <option value="USD" selected>USD(미국/달러)</option>
+          <select className={styles.receptionCountry} value={selectReceptionCountry} onChange={(e) => setSelectReceptionCountry(e.target.value)}>
             {countryList.map((country) => {
-              return <option value={country.split(0, 3)}>{country}</option>
+              return <option key={country.key} value={country.value.substring(0, 3)}>{country.value}</option>
             })}
           </select>
-        </div>
-        <div className={styles.transform_btn}>
-          <input type='button' value="변환" onClick={() => {checkValue(convertPrice)}} />
         </div>
         <div className={styles.transform_message}>
           {isVaild ? (
-            <p className={styles.outputPrice}><bold>1 USD = 1396.75</bold></p>
+            <>
+              <p className={styles.outputPrice}>{convertPrice} {selectRemittanceCountry} <br /></p>
+              <p className={styles.outputPrice}> = {exchangePrice} {selectReceptionCountry} </p>
+            </>
           ) : (
             <p className={styles.errorMessage}>{errorMassage}</p>
           )}
